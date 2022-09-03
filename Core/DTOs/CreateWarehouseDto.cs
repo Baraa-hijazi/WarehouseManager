598db6a787a0746ef04dc934c24b64820fcb1a19
Baraa Hijazi <bqumsan99@gmail.com>
2022-09-04 00:25:06 +0300

    ...

--- Core/DTOs/CreateWarehouseDto.cs
+++ Core/DTOs/CreateWarehouseDto.cs
@@ -2,13 +2,8 @@ namespace WarehouseManager.Core.DTOs;
 
 public class CreateWarehouseDto
 {
-    public CreateWarehouseDto()
-    {
-        WarehouseItems = new List<CreateWarehouseItemDto>();
-    }
-
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
 
-    public ICollection<CreateWarehouseItemDto> WarehouseItems { get; set; }
+    public ICollection<CreateWarehouseItemDto> WarehouseItems { get; set; } = new List<CreateWarehouseItemDto>();
 }
